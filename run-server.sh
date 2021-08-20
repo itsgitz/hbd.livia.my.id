2af127f4ebfe6429f91b95d081ba30a8c0c080bf
itsgitz <anggit.ginanjar@outlook.com>
2021-08-20 22:27:51 +0700

    feat: add new feature for mockup slideshow animation.

--- run-server.sh
+++ run-server.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker run --rm -d --name vuejs-web-server -p 8080:80 -v "$(pwd):/var/www/html" php:7.4-apache
+docker run --rm -d --name dear-livia.my.id -p 8080:80 -v "$(pwd):/var/www/html" php:7.4-apache
